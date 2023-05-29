38b249a5c668067c67626e011836e8721ca7d323
Konstantinos Papadopoulos <papadopouloskon2001@gmail.com>
2023-05-30 02:39:18 +0300

    Improved Mqtt Controller.

--- src/main/java/gr/upatras/mqtt/controller/controller.java
+++ src/main/java/gr/upatras/mqtt/controller/controller.java
@@ -1,107 +1,75 @@
 package gr.upatras.mqtt.controller;
 
-import java.util.Random;
-import java.util.UUID;
-
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttTopic;
+import org.eclipse.paho.client.mqttv3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
-import java.util.Scanner;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class controller implements MqttCallback {
-    MqttClient myClient;
-    MqttConnectOptions connOpt;
-    static final String M2MIO_THING = UUID.randomUUID().toString();
-    static final String BROKER_URL = "tcp://test.mosquitto.org:1883";
-    static final Boolean subscriber = false;
-    static final Boolean publisher = true;
-    private Random rnd = new Random();
-    private static final Logger log = LoggerFactory.getLogger(controller.class);
-    public static final String TOPIC = "Chat";
-
     public void connectionLost(Throwable t) {
-        log.info("Connection lost!");
     }
 
     public void deliveryComplete(IMqttDeliveryToken token) {
     }
 
     public void messageArrived(String topic, MqttMessage message) throws Exception {
-        log.info("\n");
-        log.info("-------------------------------------------------");
-        log.info("| Topic:" + topic);
-        log.info("| Message: " + new String(message.getPayload()));
-        log.info("-------------------------------------------------");
-        log.info("\n");
     }
+    @PostMapping("/")
+    @ResponseBody
+    public ResponseEntity receivePostRequest(@RequestBody String message) {
+        final String BROKER_URL = "tcp://test.mosquitto.org:1883";
+        final Logger log = LoggerFactory.getLogger(controller.class);
+        final String TOPIC = "grupatras/lab/message";
 
-    public void runClient() {
-        String clientID = M2MIO_THING;
-        connOpt = new MqttConnectOptions();
+        MqttConnectOptions connOpt = new MqttConnectOptions();
         connOpt.setCleanSession(true);
         connOpt.setKeepAliveInterval(30);
-        try {
-            myClient = new MqttClient(BROKER_URL, clientID);
+
+        try (MqttClient myClient = new MqttClient(BROKER_URL, MqttClient.generateClientId())) {
             myClient.setCallback(this);
             myClient.connect(connOpt);
+
+            log.info("Connected to " + BROKER_URL);
+            MqttTopic topic = myClient.getTopic(TOPIC);
+
+            publishMessage(topic, message, log);
+
+            disconnect(myClient);
         } catch (MqttException e) {
-            e.printStackTrace();
-            System.exit(-1);
+            log.error("An error occurred while connecting to the MQTT broker", e);
         }
-        log.info("Connected to " + BROKER_URL);
-        String myTopic = TOPIC;
-        MqttTopic topic = myClient.getTopic(myTopic);
-        // subscribe to topic if subscriber
-        if (subscriber) {
-            try {
-                int subQoS = 0;
-                myClient.subscribe(myTopic, subQoS);
-                if (!publisher) {
-                    while (true) {
-                        Thread.sleep(1000);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        if (publisher) {
-            while (true) {
-                Scanner msg = new Scanner(System.in);
-                System.out.println("Please write a message here: ");
-                String val = msg.nextLine();
-                String pubMsg = "{" + val + "}";
-                int pubQoS = 0;
-                MqttMessage message = new MqttMessage(pubMsg.getBytes());
-                message.setQos(pubQoS);
-                message.setRetained(false);
-                log.info("Publishing to topic \"" + topic + "\" qos " + pubQoS + "\" value " + val); //publish message
-                MqttDeliveryToken token = null;
-                try {
-                    token = topic.publish(message);
-                    token.waitForCompletion();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
+
+    private void publishMessage(MqttTopic topic, String message, Logger log) {
+        MqttMessage msg = new MqttMessage(message.getBytes());
+        msg.setRetained(false);
+
+        log.info("Publishing to topic \"" + topic + "\" value " + message);
         try {
-            if (subscriber) {
-                Thread.sleep(5000);
+            MqttDeliveryToken token = topic.publish(msg);
+            token.waitForCompletion();
+        } catch (MqttException e) {
+            log.error("An error occurred while publishing the message", e);
+        }
+    }
+
+    private void disconnect(MqttClient myClient) {
+        try {
+            if (myClient.isConnected()) {
+                myClient.disconnect();
             }
-            myClient.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
